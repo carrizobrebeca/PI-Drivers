0f4b04a8e130673968489ed3e04bc66d937a685a
carrizobrebeca <carrizob.rebeca@gmail.com>
2024-05-19 15:53:20 -0300

    Front

---
+++ client/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./Views/LandingPage/LandingPage";
+import HomePage from "./Views/HomePage/HomePage";
+import Details from "./Views/Details/Details";
+import Form from "./Views/Form/Form";
+import NavBar from "./Components/NavBar/NavBar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <BrowserRouter>
+         
+          <Routes>
+            <Route path="/" element={<LandingPage/>} />
+            <Route
+              path="/home"
+              element={
+                <>
+                  <NavBar />
+                  <HomePage/>
+                </>
+              }
+            />
+            <Route path="/details" element={
+             <>
+             <NavBar />
+            <Details />
+           </>} />
+            <Route path="/form" element={
+               <>
+               <NavBar />
+               <Form />
+             </>
+            } />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Redux/Reducer/reducer.js
@@ -0,0 +1,16 @@
+//importar acions-types
+
+import { RouterProvider } from "react-router-dom"
+
+//definir estado inicial del estado global
+let initialState ={
+    allDrivers: [],
+    allTeams: [],
+    filterDriver: false
+}
+
+function rootReducer(state=initialState, action) {
+    
+}
+
+export default rootReducer;
